ae68df8c4011b3fd4dba4053e3b808cc370749c8
rafaelsalass <rafaelsalass20@gmail.com>
2017-05-16 15:21:32 -0400

    change style

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router'
 
-var Examples = props => <h4>Examples component!</h4> 
+var Examples = props => {
+  return (
+    <div>
+      <h1 className="text-center">Examples!</h1>
+      <p className="text-justify">Here are some example locations, click on them to search for the
+      temperature of this places:</p>
+     <ul>
+       <li><Link to="/?place=Venezuela">Venezuela</Link></li>
+       <li><Link to="/?place=Canada">Canada</Link></li>
+       <li><Link to="/?place=Ecuador">Ecuador</Link></li>
+       <li><Link to="/?place=Madagascar">Madagascar</Link></li>
+     </ul>
+    </div>
+  )
+}; 
 
 export default Examples;
