f63bcf6a55877948fa2f64f672e612c952829ce5
rafaelsalass <rafaelsalass20@gmail.com>
2017-05-16 15:20:56 -0400

    changing style

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,29 @@
 import React from 'react'
 
-var About = props => <h4>About component</h4>
+var About = props => {
+  return (
+    <div>
+      <h1 className="text-center">About!</h1>
+      <p className="text-justify">This a simple weather app created to learn
+        about React. Interesting links:
+      </p>
+      <dl>
+        <dt>
+          <a href="https://facebook.github.io/react/">React</a>
+        </dt>
+        <dd className="text-justify">This is the framework used to create the
+        user interface of this website</dd>
+        <dt>
+          <a href="https://openweathermap.org/">OpenWeatherMap</a>
+        </dt>
+        <dd className="text-justify">Api used to search weather given a city name</dd>
+        <dt>
+          <a href="http://foundation.zurb.com/">Foundation</a>
+        </dt>
+        <dd className="text-justify">Giving style to the app</dd>
+      </dl>
+    </div>
+  );
+};
 
 export default About;
