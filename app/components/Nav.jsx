e1ea96cc3cbfe757bd69d10971222c07e6ce5e54
rafaelsalass <rafaelsalass20@gmail.com>
2017-05-16 12:40:04 -0400

    adding new styling with foundation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,17 +1,42 @@
 import React from 'react'
 import { Link, IndexLink } from 'react-router'
 
-var Nav = props => {
-  return (
-    <div>
-      <IndexLink to="/" activeClassName="active" 
-        activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="/about" activeClassName="active" 
-        activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to="/examples" activeClassName="active" 
-        activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-    </div>
-  );
-}
+var Nav = React.createClass({
+  onFormSubmit(e){
+    e.preventDefault(); 
+    console.log('form submitted');
+  },
+  render(){
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">Weather app</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" 
+                activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to="/about" activeClassName="active" 
+                activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to="/examples" activeClassName="active" 
+                activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <form onSubmit={this.onFormSubmit}>
+            <ul className="menu">
+              <li><input type="text" placeholder="Enter city name"></input></li>
+              <li><button className="button">Get Weather</button></li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    );
+  }
+});
 
 export default Nav;
