aa6485f9c9878af481ee3bef31610530d27747e6
rafaelsalass <rafaelsalass20@gmail.com>
2017-05-18 00:32:34 -0400

    removing specifig log

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ var app = express();
 app.use(express.static('public'));
 
 app.listen(3000,function(){
-  console.log('Express server is up on port 3000, from wheater');
+  console.log('Express server is up on port 3000');
 });
